3a622e15d95a6defc8b7ef866312155934705e4f
Nouradawy <nouradawyonex@gmail.com>
2025-03-11 23:18:27 +0200

    Structure Update for making project running

--- src/main/java/com/Medicare/controller/main/NewWebSpringApplication.java
+++ src/main/java/com/Medicare/controller/main/NewWebSpringApplication.java
@@ -1,4 +1,4 @@
-package com.Medicare.Medicare;
+package com.Medicare.controller.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/Medicare/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Medicare.repository;
-
-import com.Medicare.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
