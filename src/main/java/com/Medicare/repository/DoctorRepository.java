f8d06b06749cf3820d487af622ef5fc34bbb7a91
نور الدين محمد يحيى محمد العدوى <90068345+Nouradawy@users.noreply.github.com>
2025-03-20 04:27:20 +0200

    Delete src directory

--- src/main/java/com/Medicare/repository/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.Medicare.repository;
-//
-//import com.Medicare.model.Doctor;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-//}
