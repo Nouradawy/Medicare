4d80379f05de725653493d29dba7258d2edce006
نور الدين محمد يحيى محمد العدوى <90068345+Nouradawy@users.noreply.github.com>
2025-04-13 18:34:06 +0200

    install gh pagess

--- src/Homepage/App.jsx
+++ src/Homepage/App.jsx
@@ -29,13 +29,14 @@ function App() {
 
 
                     <div className="w-[260px] h-[60px] bg-[#2C2C2C] rounded-xl flex">
-                        <svg width="47" height="43" viewBox="0 0 47 43" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginTop: '17px' , marginLeft: '20px'}}>
-                            <path d="M0 38H47V43H0V38Z" fill="#FFCCFA"/>
-                            <path d="M29 26.25V23.75C29 22.4239 28.4732 21.1521 27.5355 20.2145C26.5979 19.2768 25.3261 18.75 24 18.75H15.25C13.9239 18.75 12.6521 19.2768 11.7145 20.2145C10.7768 21.1521 10.25 22.4239 10.25 23.75V26.25M31.5 10L37.75 16.25M37.75 10L31.5 16.25M24.625 8.75C24.625 11.5114 22.3864 13.75 19.625 13.75C16.8636 13.75 14.625 11.5114 14.625 8.75C14.625 5.98858 16.8636 3.75 19.625 3.75C22.3864 3.75 24.625 5.98858 24.625 8.75Z" stroke="#95D2FF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+                        <svg width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginTop: '15px' , marginLeft: '20px'}}>
+                            <path d="M0 40H47V45H0V40Z" fill="#FFCCFA"/>
+                            <path d="M18 22.5L23 18.6875L28 22.5L26.125 16.3125L31.125 12.75H25L23 6.25L21 12.75H14.875L19.875 16.3125L18 22.5ZM23 27.5C21.2708 27.5 19.6458 27.1719 18.125 26.5156C16.6042 25.8594 15.2812 24.9688 14.1562 23.8438C13.0312 22.7188 12.1406 21.3958 11.4844 19.875C10.8281 18.3542 10.5 16.7292 10.5 15C10.5 13.2708 10.8281 11.6458 11.4844 10.125C12.1406 8.60417 13.0312 7.28125 14.1562 6.15625C15.2812 5.03125 16.6042 4.14063 18.125 3.48438C19.6458 2.82812 21.2708 2.5 23 2.5C24.7292 2.5 26.3542 2.82812 27.875 3.48438C29.3958 4.14063 30.7188 5.03125 31.8438 6.15625C32.9688 7.28125 33.8594 8.60417 34.5156 10.125C35.1719 11.6458 35.5 13.2708 35.5 15C35.5 16.7292 35.1719 18.3542 34.5156 19.875C33.8594 21.3958 32.9688 22.7188 31.8438 23.8438C30.7188 24.9688 29.3958 25.8594 27.875 26.5156C26.3542 27.1719 24.7292 27.5 23 27.5Z" fill="#95D2FF"/>
                         </svg>
+
                         <div className="pl-6 pt-1">
-                            <p className="italic font-light text-[#F5F5F5] leading-none font-[Inter] text-[1rem] pt-2 ">Profiles for Every</p>
-                            <p className="italic font-light text-[#F5F5F5]  font-[Inter] text-[1rem] ">Doctor in Egypt</p>
+                            <p className="italic font-light text-[#F5F5F5] leading-none font-[Inter] text-[1rem] pt-2 ">More than 10 Million</p>
+                            <p className="italic font-light text-[#F5F5F5]  font-[Inter] text-[1rem] ">patient Ratings</p>
                         </div>
 
                     </div>
