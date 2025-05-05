fbfb8ed5bad64be4854ca845e7389ed24ca6c72f
نور الدين محمد يحيى محمد العدوى <90068345+Nouradawy@users.noreply.github.com>
2025-05-05 03:50:44 +0300

    - Adjust getAllDoctors API - city table created

--- src/main/java/com/Medicare/model/User.java
+++ src/main/java/com/Medicare/model/User.java
@@ -31,7 +31,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
-    private String profilePictureUrl;
     private String username;
     private String Password;
     private String email;
